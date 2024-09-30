b78e32cfbbedd0bf2439bfef0223d4b7f0e6f4cf
AdelaSRuizDeCortázar <sanruiade@gmail.com>
2024-09-30 08:56:31 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise17.java
+++ src/main/java/com/campusdual/classroom/Exercise17.java
@@ -1,15 +1,27 @@
 package com.campusdual.classroom;
 
+import java.util.ArrayList;
+
 public class Exercise17 {
-	public static int[] createEmptyIntArray(int positions) {}
+	public static int[] createEmptyIntArray(int positions) {
+		return new int[positions];
+	}
 
-	public static String[] createInitializedStringArray() {}
+	public static String[] createInitializedStringArray() {
+		return new String[]{"1", "2", "3"};
+	}
 
-	public static void populateArray(int[] array, int index, int value) {}
+	public static void populateArray(int[] array, int index, int value) {
+		array[index] = value;
+	}
 
-	public static int arrayLength(int[] array) {}
+	public static int arrayLength(int[] array) {
+		return array.length;
+	}
 
-	public static void showValue(int[] array, int index) {}
+	public static void showValue(int[] array, int index) {
+		System.out.println(array[index]);
+	}
 
 	public static void main(String[] args) {
 		int[] intArray = createEmptyIntArray(3);
